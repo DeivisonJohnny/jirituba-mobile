6e4062b8bac1eb159e2aa30dd896542dc8ec2ae1
Deivison Johnny <deivisonjohnny@gmail.com>
2025-01-12 11:31:57 -0300

    feat/fix: create context for permissions device / image profile in register employees

---
+++ src/app/context/permissions/index.tsx
@@ -0,0 +1,48 @@
+import { createContext, useEffect, useState } from "react";
+import { PermissionsAndroid } from "react-native";
+
+// Criação do contexto
+const PermissionsContext = createContext(null);
+
+export const PermissionsProvider = ({ children }: any) => {
+  const [permissions, setPermissions] = useState([]);
+
+  useEffect(() => {
+    const requestPermissions = async () => {
+      try {
+        const granted = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.NEARBY_WIFI_DEVICES,
+          PermissionsAndroid.PERMISSIONS.CAMERA,
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.READ_CONTACTS,
+          PermissionsAndroid.PERMISSIONS.WRITE_CONTACTS,
+          PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION,
+        ]);
+
+        for (const [permission, result] of Object.entries(granted)) {
+          if (result === PermissionsAndroid.RESULTS.GRANTED) {
+            console.log(`Permissão concedida - ${permission}`);
+            setPermissions((prev) => [...prev, permission] as any);
+          } else {
+            console.log(`Permissão negada - ${permission}`);
+          }
+        }
+
+        console.log("result ->>>", PermissionsAndroid.RESULTS.GRANTED);
+      } catch (error) {
+        console.warn(error);
+      }
+    };
+
+    requestPermissions();
+  }, []);
+
+  return (
+    <PermissionsContext.Provider value={permissions as any}>
+      {children}
+    </PermissionsContext.Provider>
+  );
+};
+
+export default PermissionsContext;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,16 +1,17 @@
-import styled from "styled-components/native";
 import AppThemeContext from "./context/theme-context";
 import Login from "./login";
 import SplashScreen from "../components/splash-screen";
-import { StatusBar } from "expo-status-bar";
+import { PermissionsProvider } from "./context/permissions";
 
 const App = () => {
   return (
-    <AppThemeContext>
-      <SplashScreen>
-        <Login></Login>
-      </SplashScreen>
-    </AppThemeContext>
+    <PermissionsProvider>
+      <AppThemeContext>
+        <SplashScreen>
+          <Login></Login>
+        </SplashScreen>
+      </AppThemeContext>
+    </PermissionsProvider>
   );
 };
 
