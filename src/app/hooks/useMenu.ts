cd4399b12c470739c44c4d300d690da2286e6941
Deivison Johnny <deivisonjohnny@gmail.com>
2025-01-13 14:41:33 -0300

    feat: created list menus

---
+++ src/app/hooks/useMenu.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+import MenuItens from "../utils/listMenu";
+
+type Categorys = "Principal" | "Sobre o App";
+
+export type itemMenu = {
+  title: string;
+  icon: string;
+  uri: string;
+  prefixIcon?: "default" | "outline" | "sharp";
+
+  sizeIcon?: number; //default 20
+  sizeTitle?: number;
+  widthTitle?: number;
+  widthIcon?: number;
+  category: Categorys;
+};
+
+export const CategorysMenu = ["Principal", "Sobre o App"] as Categorys[];
+
+export function useMenu() {
+  const [itensMenu, setItensMenu] = useState<itemMenu[]>(MenuItens);
+
+  const addItemMenu = (item: itemMenu) => {
+    setItensMenu((prevItens) => [
+      ...prevItens,
+      {
+        title: item.title,
+        uri: item.uri,
+        icon: item.icon + `${item.prefixIcon ? "-" + item.prefixIcon : ""}`,
+        prefixIcon: item.prefixIcon,
+        sizeIcon: item.sizeIcon ?? 20,
+        sizeTitle: item.sizeTitle,
+        widthTitle: item.widthTitle,
+        widthIcon: item.widthIcon,
+        category: item.category,
+      },
+    ]);
+  };
+
+  return { itensMenu, addItemMenu };
+}
--- src/app/utils/listMenu.ts
+++ src/app/utils/listMenu.ts
@@ -1,44 +1,12 @@
-import { useState } from 'react';
+import { itemMenu } from "../hooks/useMenu";
 
-type Categorys = "Principal" | "Sobre o App"
+const MenuItens = [
+  {
+    title: "Funções",
+    category: "Principal",
+    icon: "person",
+    uri: "pages/roles-employees/",
+  },
+] as itemMenu[];
 
-export type itemMenu = {
-  title: string;
-  icon: string;
-  prefixIcon?: "default" | "outline" | "sharp";
-
-  sizeIcon?: number; //default 20
-  sizeTitle?: number;
-  widthTitle?: number;
-  widthIcon?: number;
-  category: Categorys;
-};
-
-export const CategorysMenu = [ 
-  "Principal" ,
-  "Sobre o App"
-] as Categorys[]
-
-
-export function useItemMenu() {
-  const [itensMenu, setItensMenu] = useState<itemMenu[]>([]);
-
-
-  const addItemMenu = (item: itemMenu) => {
-    setItensMenu(prevItens => [
-      ...prevItens,
-      {
-        title: item.title,
-        icon: item.icon + `${item.prefixIcon ? '-' + item.prefixIcon : ''}`,
-        prefixIcon: item.prefixIcon,
-        sizeIcon: item.sizeIcon ?? 20,
-        sizeTitle: item.sizeTitle,
-        widthTitle: item.widthTitle,
-        widthIcon: item.widthIcon,
-        category: item.category,
-      },
-    ]);
-  };
-
-  return { itensMenu, addItemMenu };
-}
+export default MenuItens;
