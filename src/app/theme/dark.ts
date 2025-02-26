28aa41e2b2637e8f1eb54f0642ae0a40d09099c6
Deivison Johnny <deivisonjohnny@gmail.com>
2025-02-26 13:46:16 -0300

    refactor

--- src/app/theme/dark.ts
+++ src/app/theme/dark.ts
@@ -1,10 +1,3 @@
 export default {
-    background: '#0E0E12',
-    color: '#fff',
-    colorLabel: '#666680',
-
-    login: {
-        colorButtonSubmit: '#ff7966',
-        colorShadowBtnSubmit: '#ff78667a'
-    }
-}
+  baseBackground: "#0e0e12",
+};
