d3a01c367e7cf01135e538333896d49502cb6218
Deivison Johnny <deivisonjohnny@gmail.com>
2025-02-28 22:10:58 -0300

    feat: add atributte color border the card

--- src/components/profile/index.tsx
+++ src/components/profile/index.tsx
@@ -3,23 +3,21 @@ import styled from "styled-components/native";
 
 interface ProfileCardProps {
   size?: number;
+  borderColor?: string;
 }
 
 const ProfileCard = (props: ProfileCardProps) => {
   return (
-    <ProfileImage style={{borderRadius: 100}}>
+    <ProfileImage
+      style={{ borderRadius: 100, borderColor: props.borderColor || "gray" }}
+    >
       <ImageUser
-        style={
-          [
-            props.size
-              ? { width: props.size, height: props.size }
-              : { width: 40, height: 40 }
-            ,
-
-            {borderRadius: 100}
-          ]
-            
-        }
+        style={[
+          props.size
+            ? { width: props.size, height: props.size }
+            : { width: 40, height: 40 },
+          { borderRadius: 100 },
+        ]}
         source={require("../../app/assets/img/foto-perfil.jpg")}
       />
     </ProfileImage>
@@ -32,7 +30,7 @@ const ProfileImage = styled.TouchableOpacity`
   padding: 2px;
   border: 2px solid gray;
   width: fit-content;
-  `;
+`;
 const ImageUser = styled.Image`
   width: 40px;
   height: 40px;
