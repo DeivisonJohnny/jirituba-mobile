4e3fcbb2a00cd77c0156027592ccc0abf34b0416
Deivison Johnnny <deivisonjohnny@gmail.com>
2025-04-14 13:03:08 -0300

    fix: port url the api

--- src/app/config/api/client.ts
+++ src/app/config/api/client.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.API_DEFAULT_URL || 'http://10.102.0.22:3443',
-    timeout: 10000,
-    headers: { "Content-Type": 'application/json' }
-  });
-  
+  baseURL: process.env.API_DEFAULT_URL || "http://10.102.0.22:3000",
+  timeout: 10000,
+  headers: { "Content-Type": "application/json" },
+});
+
 export default api;
